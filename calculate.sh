22e10562bc4de5d39d8ba99883e1a0c2e3d4a397
pruzanov <pruzanov@oicr.on.ca>
2021-06-03 16:31:54 -0400

    Last try before I drastically simplify this test to make it go through

--- calculate.sh
+++ calculate.sh
@@ -3,8 +3,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-cd $1
-
 module load jq
 # remove the Picard header because it includes temporary paths
 find . -type f -exec jq 'del(.picard | .header)' {} \; | python3 -mjson.tool --sort-keys
