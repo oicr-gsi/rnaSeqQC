60a8d01dc0c09e2a9316bfc75c7f83d274bdcf6d
pruzanov <pruzanov@oicr.on.ca>
2022-01-29 23:06:46 -0500

    RT script

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -6,4 +6,4 @@ set -o pipefail
 module load jq
 # remove the Picard header because it includes temporary paths
 for f in $(find . -xtype f | grep -v star);do jq 'del(.picard | .header)' $f | python3 -mjson.tool --sort-keys;done
-for f in $(find . -xtype f | grep star);do jq 'del(.picard | .header)' $f | python3 -mjson.tool --sort-keys | egrep -v "\"[[:digit:]]*\": [[:digit:]]";done
+for f in $(find . -xtype f | grep star);do jq 'del(.picard | .header)' $f | grep -v "\"[[:digit:]]*\": [[:digit:]]" | python3 -mjson.tool --sort-keys;done
