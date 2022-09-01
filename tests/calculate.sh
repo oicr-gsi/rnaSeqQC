b9332324a7845b5fe8d4613c9c037743316e4b49
Lawrence Heisler <lawrence.heisler@oicr.on.ca>
2022-09-01 12:21:32 -0400

    updated calculate script for regression tests; update metrics file on cluster to new expected values

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,4 +5,14 @@ set -o pipefail
 
 module load jq
 # remove the Picard header because it includes temporary paths
-for f in $(find . -xtype f);do jq 'del(.picard | .header)' $f --sort-keys | md5sum;done
+#for f in $(find . -xtype f);do jq 'del(.picard | .header)' $f --sort-keys | md5sum;done
+
+for f in $(find . -xtype f);do jq '.bamqc' $f --sort-keys | md5sum;done
+for f in $(find . -xtype f);do jq '.bamqc_transcriptome."insert size average"' $f | md5sum;done
+#for f in $(find . -xtype f);do jq '.picard' $f --sort-keys | md5sum;done
+for f in $(find . -xtype f);do jq '.rrna_contamination' $f --sort-keys | md5sum;done
+for f in $(find . -xtype f);do jq '.unique_reads' $f --sort-keys | md5sum;done
+
+
+
+
